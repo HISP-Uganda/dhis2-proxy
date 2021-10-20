96734d1c4332209d730b4f6e840de7d0ce5706a4
Charles Olupot <olupotcharles@gmail.com>
2021-10-20 12:38:15 +0300

    Validation of certificate

--- services/certificates.service.js
+++ services/certificates.service.js
@@ -52,7 +52,7 @@ module.exports = {
               Date.parse(data.DOSE2.eventDate)
             )},${data.DOSE2.orgUnitName},${
               data.DOSE2.districtName || ""
-            }\n\nClick to verify\nhttps://services.dhis2.hispuganda.org/certificates/validate/${
+            }\n\nClick to verify\nhttps://epivac.health.go.ug/certificates/#/validate/${
               data.trackedEntityInstance
             }`,
             { margin: 0 }
@@ -87,7 +87,7 @@ module.exports = {
               Date.parse(data.DOSE2.eventDate)
             )},${data.DOSE2.orgUnitName},${
               data.DOSE2.districtName || ""
-            }\n\nClick to verify\nhttps://services.dhis2.hispuganda.org/certificates/validate/${
+            }\n\nClick to verify\nhttps://epivac.health.go.ug/certificates/#/validate/${
               data.trackedEntityInstance
             }`,
             { margin: 0 }
